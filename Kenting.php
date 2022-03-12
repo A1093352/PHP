d864acd7441b586ea66acf33b62c735b4b1ce28f
A1093352 <100329022+A1093352@users.noreply.github.com>
2022-03-12 16:02:22 +0800

    Add files via upload

---
+++ Kenting.php
@@ -0,0 +1,80 @@
+<html>
+
+<head>
+
+<title>墾丁三日遊！</title>
+<link rel="icon" href="/img/nuk.bmp" type="image/x-icon" />
+</head>
+
+<body bgcolor='	#FFFFAA'>
+
+<center><h1>墾丁三日遊！</h1></center>
+
+<h2>
+<ol>
+<li>行程內容
+<p>
+第一天行程：<font color='blue'>鵝鑾鼻燈塔</font><br/>
+第二天行程：<font color='blue'>墾丁海生館</font><br/>
+第三天行程：<font color='blue'>愛琴海岸咖啡</font><br/>
+</br>
+
+<a href="https://www.google.com.tw/maps/place/%E9%B5%9D%E9%91%BE%E9%BC%BB%E7%87%88%E5%A1%94/@21.9024336,120.8519656,18.15z/data=!4m9!1m2!2m1!1z5aK-5LiB6bWd6ZG-6by754eI5aGU!3m5!1s0x3471b1150ebf5f65:0x5757d23a8bc2a410!8m2!3d21.9022141!4d120.8526431!15sChXlor7kuIHptZ3pkb7pvLvnh4jloZRaGyIZ5aK-5LiBIOm1nSDpkb4g6by7IOeHiOWhlJIBEnRvdXJpc3RfYXR0cmFjdGlvbpoBJENoZERTVWhOTUc5blMwVkpRMEZuU1VOTGFqaGxSM0IzUlJBQg?hl=zh-TW" target="new"><img src="https://www.taiwan.net.tw/pic.ashx?qp=1/big_journey/pic_69_2.jpg&sizetype=2" width="20%"></a>
+&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+<a href="https://www.google.com/maps/place/%E5%9C%8B%E7%AB%8B%E6%B5%B7%E6%B4%8B%E7%94%9F%E7%89%A9%E5%8D%9A%E7%89%A9%E9%A4%A8/@22.0460173,120.6989058,15z/data=!4m2!3m1!1s0x0:0x3fc2d412a3a7a1c4?sa=X&hl=zh-TW&ved=2ahUKEwi2gdH1gMD2AhUSAN4KHRcHC_cQ_BJ6BAgbEAU" target="new"><img src="https://image.kkday.com/v2/image/get/w_960%2Cc_fit%2Cq_55%2Ct_webp/s1.kkday.com/product_9468/20190225091057_q1e4v/jpg" width="20%" height="28%"></a>
+</br>
+<a><font size="4">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp鵝鑾鼻燈塔</font></a>
+<a><font size="4">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp墾丁海生館</font></a>
+</br>
+</br>
+<font color="red" size="4">(點擊圖片查看景點資訊)</font>
+</p>
+
+
+<li>資料填寫
+
+<p>
+<form action="hello.php" method="post">
+Name: <input type="text" name="userName" placeholder="請輸入姓名" required>
+<br/>Email: <input type="email" name="email" required>
+<br/>Tel: <input type="number" name="tel" required>
+<br/>Gender: <input type="radio" name="gender" checked>Male<input type="radio" name="gender">Female<input type="radio" name="gender">不願意透漏
+<br/>Food Preference: <input type="checkbox" name="meat">葷食<input type="checkbox" name="vegetable">素食<input type="checkbox" name="noBeef">不吃牛肉<input type="checkbox" name="noSeafood">不吃海鮮<input type="checkbox" name="other">其他
+</br>
+T-shirt size: <select name="size">
+<option> S
+<option> M
+<option> L
+<option> XL 
+</select>
+
+<br/>T-shirt color: <input type="color" name="color">
+<br/>Birthday: <input type="date" name="date">
+<br/>Tickets: <input type="number" name="num">
+<br/>
+</p>
+</ol>
+</h2>
+
+<center><input type="submit"></center>
+</form>
+
+<div align="center">
+<h2>T-shirt size 對照表</h2>    
+<table border="2" width="40%">
+<tr><th></th><th>S</th><th>M</th><th>L</th><th>XL</th></tr>
+<tr><td>長</td><td>25</td><td>26</td><td>27</td><td>28</td></tr>
+<tr><td>寬</td><td>19</td><td>20</td><td>21</td><td>22</td></tr>
+<tr><td>高</td><td>38</td><td>40</td><td>42</td><td>44</td></tr>
+</table>
+</div>
+
+
+</body>
+
+
+
+
+
+
+</html>
