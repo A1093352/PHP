a3d9a47e31804519670d1d7046566caea7cc3465
A1093352 <100329022+A1093352@users.noreply.github.com>
2022-03-20 14:01:51 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,31 +1,53 @@
 <html>
 
 <head>
-<title>PHP Week1 Exercise</title>
+    <title>登入或註冊！</title>
 </head>
 
-<body bgcolor="#C4E1FF">
+<body bgcolor = "#fbfeec">
+   <div align = "center">
+       <br/>
+        <p><font size = "7"><b>用戶登入</b></font></p>
+        <form action = "Kenting.php" method = "post" style = "width: 80%;" enctype = "multipart/form-data">
+            <br/><br/></br><br/>
+            <font size = "6"><b>帳號：</b></font>
+            <input type = "text" name = "userName" style="font-size:20px" placeholder="請輸入帳號" required>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <font size = "6"><b>密碼：</b></font>
+            <input type="password" name="passwd" style="font-size:20px" required>
+            </br></br></br>
+            <button type="submit">Login</button>
+        </form>
+    </div>
 
 
-<?php
 
-echo date("Y,m,d");
-echo "</br>";
-echo "<h1><b><i>Hello PHP!</i></b></h1>";
-$i=1;
-$j=2;
-echo "i=1, j=2";
-echo "</br>";
-echo "i + j = ";
-echo $i+$j;
-echo "</p>";
-echo "A1093352韓昀珊";
-
-
-
-?>
 
 </body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
